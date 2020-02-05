9e99f3444c85dd4c754a3716b7beaf0b60c1b058
Jorge Nogueira <jlnpinheiro@gmail.com>
2020-02-04 23:01:08 -0300

    Ajustes

--- src/_docker/bat-docker-compose-down.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Executando docker-compose -f docker-compose.yml down ...
-docker-compose -f docker-compose.yml down
-echo.
-pause
--- src/_docker/bat-docker-compose-up.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Executando docker-compose -f docker-compose.yml up -d ...
-docker-compose -f docker-compose.yml -p bufunfa up -d
-echo.
-pause
